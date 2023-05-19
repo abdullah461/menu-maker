e967b267d0cbd1320992aefcbf2e1d2df9691cea
abdull461 <abdullahabubakar461@gmail.com>
2023-05-19 18:12:23 +0100

    comments for user model

--- website/models.py
+++ website/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
+# table for the user(admin)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
